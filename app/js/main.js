5066b44e48d72e92a411be081d9286350d820d2a
qwelp <qwelp@mail.ru>
2015-07-29 01:04:54 +0300

    Добавление дополнительных шрифтов

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
+var qwelp = (function(){
+
+	var _footerId = '#mainFooter',
+		_mainId = "#main",
+		_mainPaddingBottom = 40;
+
+	function init(){
+		_footerPosition(_footerId, _mainId, _mainPaddingBottom);
+	}
+
+	function _footerPosition(footerId, mainId, mainPaddingBottom){
+		$(footerId).css("margin-top", "-" + $(footerId).height() + "px");
+		$(mainId).css("margin-bottom", ($(footerId).height()+mainPaddingBottom) + "px");
+	}
+
+	return {
+		init: init
+	};
+
+}());
+
 $(document).ready(function($) {
 	/*console.log("jobs!");*/
+	qwelp.init();
 });
