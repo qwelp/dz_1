acd22936d01818da436125b0929190d1a3e849e6
qwelp <qwelp@mail.ru>
2015-07-29 23:31:15 +0300

    Закончен рабочий день

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,35 @@
 var qwelp = (function(){
 
 	var _footerId = '#mainFooter',
-		_mainId = "#main",
-		_mainPaddingBottom = 40;
+		_mainId = "#main";
+		_mainPaddingBottom = 40; /*отступ снизу от main в px*/
 
 	function init(){
 		_footerPosition(_footerId, _mainId, _mainPaddingBottom);
+		_alignVertical();
+		_alignCenter();
 	}
 
-	function _footerPosition(footerId, mainId, mainPaddingBottom){
-		$(footerId).css("margin-top", "-" + $(footerId).height() + "px");
-		$(mainId).css("margin-bottom", ($(footerId).height()+mainPaddingBottom) + "px");
+	/*Позиция Footer в зависимости от высоты*/
+	function _footerPosition(footer, main, paddingBottom) {
+		$(footer).css("margin-top", "-" + $(footer).height() + "px");
+		$(main).css("padding-bottom", ($(footer).height()+paddingBottom) + "px");
+	}
+
+	/*Высчитавает половину у высоты класса .posVert для выравнивания по вертикали*/
+	function _alignVertical() {
+		$(".posVert").each(function(){
+			var $this = $(this);
+			$this.css("margin-top", "-" + ($this.height()/2) + "px");
+		});
+	}
+
+	/*Высчитавает половину у ширины класса .posCenter для выравнивания по горизонтали*/
+	function _alignCenter() {
+		$(".posCenter").each(function(){
+			var $this = $(this);
+			$this.css("margin-left", "-" + ($this.width()/2) + "px");
+		});
 	}
 
 	return {
@@ -22,4 +41,8 @@ var qwelp = (function(){
 $(document).ready(function($) {
 	/*console.log("jobs!");*/
 	qwelp.init();
+
+	if (!Modernizr.input.placeholder){
+		$('input, textarea').placeholder();
+	}
 });
