e25a1cef44e99395ede0254680c7f2d26b3d17b6
qwelp <qwelp@mail.ru>
2015-08-09 16:46:40 +0300

    qtip, bPopup, доработана адаптивность

--- app/js/main.js
+++ app/js/main.js
@@ -13,7 +13,6 @@ var qwelp = (function(){
 			e.preventDefault();
 			_showMenu(".menuVert");
 		});
-		_pageActive();
 	}
 
 	/*Позиция Footer в зависимости от высоты*/
@@ -30,18 +29,7 @@ var qwelp = (function(){
 		}
 	}
 	function _showMenu(element) {
-		$(element).toggle();
-	}
-	function _pageActive() {
-		var url = window.location.pathname;
-		$(".menuVertList li").removeClass("active");
-		$(".menuVertList li a").each(function(index, el) {
-			var $this = $(this);
-			if("/" + $this.attr("href") === url)
-			{
-				$this.parent().addClass('active');
-			}
-		});
+		$(element).toggle("slow");
 	}
 
 	return {
@@ -66,9 +54,19 @@ $(document).ready(function($) {
 			modalColor:"#58697a",
 			opacity:.75,
 			closeClass:"formClose",
-			transition: 'slideDown'
+			transition: 'slideDown',
+			onClose: function() {
+				$('#addProject [qtip-position], #addProject .formFieldUpload').removeClass('has-error').trigger('hideTooltip');
+			}
 		});
 	});
+	if($('#formAut').length > 0)
+	{
+		$('#formAut').bPopup({
+			modalColor: false,
+			modalClose: false,
+		});
+	}
 	$(".form input[type=file]").change(function() {
 		var $this = $(this),
 			fileName = $this.val(),
@@ -81,6 +79,95 @@ $(document).ready(function($) {
 		} else {
 			msg.text(fileArray[fileArray.length-1]);
 		}
+		$this.parents(".formFieldUpload").removeClass('has-error').end().trigger('hideTooltip');;
 	});
+
+
+
+	$("body").on("submit", ".form", function(e) {
+		e.preventDefault();
+		var $this = $(this),
+			elementLength = 0;
+
+		$("[qtip-position]", this).each(function(i){
+			var element = $(this),
+				pos = element.attr('qtip-position');
+				if($(window).width() < 730)
+				{
+					pos = element.attr('qtip-position-mobile');
+				}
+
+			if(element.val().length === 0) {
+				element.addClass('has-error');
+				if(element.attr("type") === "file") {
+					element.parents(".formFieldUpload").addClass('has-error');
+				}
+				createQtip(element, pos);
+				elementLength = elementLength+1;
+			}
+		});
+		if(elementLength === 0) {
+			$this.parentsUntil("form").find(".error").show();
+		}
+	});
+	$("body").on('click', '.errorClose', function(e) {
+		e.preventDefault();
+		$(this).parent("div").hide();
+	});
+	$("body").on("reset", function() {
+		var $this = $(this);
+		$this.parentsUntil("form").find(".error").hide();
+		$this.parentsUntil("form").find("[qtip-position]").each(function(i){
+			$(this).removeClass('has-error').trigger('hideTooltip');
+		});
+	});
+	$("body").on("keyup", "[qtip-position]", function(e) {
+		$(this).removeClass('has-error');
+	});
+	function createQtip(el, position){
+		if (position === 'right') {
+			position = {
+				my: 'left center',
+				at: 'right center'
+			};
+		}
+		else if(position === 'bottom') {
+			position = {
+				my: 'bottom center',
+				at: 'top center'
+			};
+		}
+		else {
+			position = {
+				my: 'right center',
+				at: 'left center'
+			};
+		}
+
+		el.qtip({
+			content: {
+				text: function(){
+					return $(this).attr('qtipcontent')
+				}
+			},
+			show: {
+				event: 'show'
+			},
+			hide: {
+				event: 'keydown hideTooltip'
+			},
+			position: position,
+			style: {
+				classes: 'qtip-red qtip-rounded'
+			}
+		}).trigger('show');
+	};
+
+	$(".imgWorkList li").hover(function() {
+		$(".imgWorkListActiveName", this).show();
+	}, function() {
+		$(".imgWorkListActiveName", this).hide();
+	});
+
 
 });
